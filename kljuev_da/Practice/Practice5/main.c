90997ba4a2e8c23e43273b79c5e2ad9afb8971b6
KljuevD <43135919+KljuevD@users.noreply.github.com>
2018-12-03 14:27:38 +0300

    Practice5

--- kljuev_da/Practice/Practice5/main.c
+++ kljuev_da/Practice/Practice5/main.c
@@ -1,14 +1,14 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <locale.h>
 #define N 5
 #define Range 100
 
-
-
+void Print(int a[], int n);
+/*
 void Input(int a[], int n, int mode)
 {
     int i, j, f;
-    printf("Введите массив\n");
+    printf("Введите массив\n");                 ///////////////////////
     if (mode != 3)
         for (i = 0; i < n; i++)
             scanf("%d", &(a[i]));
@@ -24,8 +24,16 @@ void Input(int a[], int n, int mode)
                 continue;
             i++;
         }
-}
+}*/
 
+void Input(int a[], int n, int mode)
+{
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
+}
 void ChooseSort(int a[], int n)
 {
     int i, j, min, ind;
@@ -64,12 +72,12 @@ void InsertSort(int a[], int n)
 void BubbleSort(int a[], int n)
 {
     int i, j, tmp;
-    for (i = 0; i < n -1; i++)
+    for (i = 0; i < n - 1; i++)
     {
         for (j = 1; j < n - i; j++)
             if (a[j - 1] > a[j])
             {
-                tmp = a[j-1];
+                tmp = a[j - 1];
                 a[j - 1] = a[j];
                 a[j] = tmp;
             }
@@ -92,6 +100,64 @@ void CountingSort(int a[], int n)
             a[ind++] = i + min;
 }
 
+void QuickSplit(int a[], int *i, int *j, int p)
+{
+    do
+    {       
+        while (a[*i] < p)
+            (*i)++;
+        while (a[*j] > p)
+            (*j)--;
+        if (*i <= *j)
+        {
+            int tmp = a[*i];
+            a[*i] = a[*j];
+            a[*j] = tmp;
+        }
+    } while (*i < *j);
+}
+
+void QuickSort(int a[], int n1, int n2)
+{
+    int m = (n1 + n2) / 2;
+    int i = n1, j = n2;
+    QuickSplit(a, &i, &j, a[m]);
+    if (i > n1)
+        QuickSort(a, n1, i - 1);
+    if (j < n2)
+        QuickSort(a, j + 1, n2);
+}
+
+void Merge(int a[], int l, int m, int r)
+{
+    int i = l, j = m, c = 0, tarr[N], k = 0;
+    while (i < m && j < r)
+    {
+        if (a[i] < a[j])
+            tarr[c++] = a[i++];
+        else
+            tarr[c++] = a[j++];
+    }
+    while (i < m)
+        tarr[c++] = a[i++];
+    while (j < r)
+        tarr[c++] = a[j++];
+    while (k < r - l)
+        a[i] = tarr[i];
+    Print(a, r - l);
+}
+
+void MergeSort(int a[], int l, int r)
+{
+    int m = (l + r) / 2;
+    if (l >= r)
+        return;
+    printf("ff\n");     ///////////
+    MergeSort(a, l, m);
+    MergeSort(a, m, r);
+    Merge(a, l, m, r);
+}
+
 void Print(int a[], int n)
 {
     int i;
@@ -104,13 +170,14 @@ void main()
     int mode, a[N];
     setlocale(LC_ALL, "Rus");
     printf("Выберите режим:\n0 – выбором, 1 – вставкой, "
-      "2 – пузырьком, 3 – подсчётом\n");
+      "2 – пузырьком, 3 – подсчётом,\n4 – быстрая, "
+      "5 – слиянием\n");
     do
     {
         scanf("%d", &mode);
-    } while (mode < 0 || mode > 3);
+    } while (mode < 0 || mode > 5);
     Input(a, N, mode);
-    printf("Сортировка по возрастанию\n");
+    printf("\nСортировка по возрастанию\n\n");
     switch (mode)
     {
     case 0:
@@ -125,6 +192,11 @@ void main()
     case 3:
         CountingSort(a, N);
         break;
+    case 4:
+        QuickSort(a, 0, N - 1);
+        break;
+    case 5:
+        MergeSort(a, 0, N - 1);
     }
     Print(a, N);
 }
