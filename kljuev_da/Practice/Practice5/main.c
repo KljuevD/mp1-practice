2a1bdcf4e46544cf5340f08f2855a78a1906af7d
KljuevD <aloky.2000@gmail.com>
2018-12-24 01:28:06 +0300

    Practice5

--- kljuev_da/Practice/Practice5/main.c
+++ kljuev_da/Practice/Practice5/main.c
@@ -2,176 +2,163 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <windows.h>
+#include <time.h>
 #define MAX_LEN 256
-#define MAX_AMOUNT 256
-#define N 5
-#define Range 100
+#define MAX_AMOUNT 1024
+#define Range 16777216
 
-void QuickSplit(int *a, int *i, int *j, int p);
-void Merge(int *a, int l, int m, int r);
+void QuickSplit(ULONGLONG *size_copy, int *i, int *j, ULONGLONG base);
+void Merge(ULONGLONG *size_copy, int l, int m, int r);
 
-void Input(int *a, int n, int mode)
-{
-    int i, j, f;
-    printf("Введите массив\n");
-    if (mode != 3)
-        for (i = 0; i < n; i++)
-            scanf("%d", &(a[i]));
-    else
-        for (i = 0; i < n;)
-        {
-            scanf("%d", &(a[i]));
-            f = 0;
-            for (j = 0; j < i; j++)
-                if (a[j] - a[i] > Range - 1 || a[i] - a[j] > Range - 1)
-                    f = 1;
-            if (f == 1)
-                continue;
-            i++;
-        }
-}
-void Print(int *a, int n)
+void Print(wchar_t **name, ULONGLONG *size, int *index, int n, int order, double time)
 {
     int i;
-    for (i = 0; i < n; i++)
-        printf("%d\n", a[i]);
+    if (n == 0)
+        printf("В папке нет элементов\n");
+    else
+    {
+     if (order == 1)
+          for (i = 0; i < n; i++)
+               wprintf(L"Файл: %s Размер: %lli\n",
+                  name[index[i]], size[index[i]]);
+        else
+           for (i = n - 1; i > -1; i--)
+               wprintf(L"Файл: %s Размер: %lli\n",
+                  name[index[i]], size[index[i]]);
+       printf("Время сортировки %lf секунд(ы)\n", time);
+    }
 }
-void ChooseSort(int *a, int n)
+void ChooseSort(ULONGLONG *size_copy, int n)
 {
-    int i, j, min, ind;
+    int i, j, ind;
+    ULONGLONG min;
     for (i = 0; i < n - 1; i++)
     {
-        min = a[i];
+        min = size_copy[i];
         ind = i;
         for (j = i + 1; j < n; j++)
         {
-            if (a[j] < min)
+            if (size_copy[j] < min)
             {
-                min = a[j];
+                min = size_copy[j];
                 ind = j;
             }
         }
-        a[ind] = a[i];
-        a[i] = min;
+        size_copy[ind] = size_copy[i];
+        size_copy[i] = min;
     }
 }
-void InsertSort(int *a, int n)
+void InsertSort(ULONGLONG *size_copy, int n)
 {
-    int i, j, tmp;
+    int i, j;
+    ULONGLONG tmp;
     for (i = 1; i < n; i++)
     {
-        tmp = a[i];
+        tmp = size_copy[i];
         j = i - 1;
-        while (j >= 0 && a[j] > tmp)
+        while (j >= 0 && size_copy[j] > tmp)
         {
-            a[j + 1] = a[j];
-            a[j--] = tmp;
+            size_copy[j + 1] = size_copy[j];
+            size_copy[j] = tmp;
         }
     }
 }
-void BubbleSort(int *a, int n)
+void BubbleSort(ULONGLONG *size_copy, int n)
 {
-    int i, j, tmp;
+    int i, j;
+    ULONGLONG tmp;
     for (i = 0; i < n - 1; i++)
     {
         for (j = 1; j < n - i; j++)
-            if (a[j - 1] > a[j])
+            if (size_copy[j - 1] > size_copy[j])
             {
-                tmp = a[j - 1];
-                a[j - 1] = a[j];
-                a[j] = tmp;
+                tmp = size_copy[j - 1];
+                size_copy[j - 1] = size_copy[j];
+                size_copy[j] = tmp;
             }
     }
 }
-void CountingSort(int *a, int n)
+void CountingSort(ULONGLONG *size_copy, int n)
 {
-    int *count, i, j, min = a[0], ind = 0;
+    int *count, i, j, ind = 0;
+    ULONGLONG min = size_copy[0];
     count = (void*)malloc(Range * sizeof(int));
     memset(count, 0, Range * sizeof(int));
     for (i = 0; i < n; i++)
     {
-        if (a[i] < min)
-            min = a[i];
+        if (size_copy[i] < min)
+            min = size_copy[i];
     }
     for (i = 0; i < n; i++)
-        count[a[i] - min]++;
+        count[size_copy[i] - min]++;
     for (i = 0; i < Range; i++)
         for (j = 0; j < count[i]; j++)
-            a[ind++] = i + min;
+        {
+            size_copy[ind] = i + min;
+        }
     free(count);
 }
-void QuickSort(int *a, int n1, int n2)
+void QuickSort(ULONGLONG *size_copy, int n1, int n2)
 {
-    int m = (n1 + n2) / 2;
+    ULONGLONG m = (n1 + n2) / 2;
     int i = n1, j = n2;
-    QuickSplit(a, &i, &j, a[m]);
+    QuickSplit(size_copy, &i, &j, size_copy[m]);
     if (i > n1)
-        QuickSort(a, n1, i - 1);
+        QuickSort(size_copy, n1, i - 1);
     if (j < n2)
-        QuickSort(a, j + 1, n2);
+        QuickSort(size_copy, j + 1, n2);
 }
-void MergeSort(int *a, int l, int r)
+void QuickSplit(ULONGLONG *size_copy, int *i, int *j, ULONGLONG base)
+{
+    do
+    {
+        while (size_copy[*i] < base)
+            (*i)++;
+        while (size_copy[*j] > base)
+            (*j)--;
+        if (*i <= *j)
+        {
+            ULONGLONG tmp = size_copy[*i];
+            size_copy[*i] = size_copy[*j];
+            size_copy[*j] = tmp;
+        }
+    } while (*i < *j);
+}
+void MergeSort(ULONGLONG *size_copy, int l, int r)
 {
     int m = (l + r) / 2;
     if (l >= r)
         return;
-    MergeSort(a, l, m);
-    MergeSort(a, m + 1, r);
-    Merge(a, l, m, r);
+    MergeSort(size_copy, l, m);
+    MergeSort(size_copy, m + 1, r);
+    Merge(size_copy, l, m, r);
 }
-void QuickSplit(int *a, int *i, int *j, int p)
+void Merge(ULONGLONG *size_copy, int l, int m, int r)
 {
-    do
-    {
-        while (a[*i] < p)
-            (*i)++;
-        while (a[*j] > p)
-            (*j)--;
-        if (*i <= *j)
-        {
-            int tmp = a[*i];
-            a[*i] = a[*j];
-            a[*j] = tmp;
-        }
-    } while (*i < *j);
-}
-void Merge(int *a, int l, int m, int r)
-{
-    int i = l, j = m + 1, s = l, *c, k = l;
-    c = (int*)malloc(N * sizeof(int));
+    int i = l, j = m + 1, s = l,  k = l;
+    ULONGLONG *c;
+    c = (ULONGLONG*)malloc(MAX_AMOUNT * sizeof(ULONGLONG));
     while (i < m + 1 && j < r + 1)
-        c[s++] = a[i] < a[j] ? a[i++] : a[j++];
+        c[s++] = size_copy[i] < size_copy[j] ? size_copy[i++] : size_copy[j++];
     while (i < m + 1)
-        c[s++] = a[i++];
+        c[s++] = size_copy[i++];
     while (j < r + 1)
-        c[s++] = a[j++];
+        c[s++] = size_copy[j++];
     while (k < r + 1)
-        a[k] = c[k++];
+        size_copy[k] = c[k++];
     free(c);
 }
-int ListDirectoryContents(const wchar_t *sDir, int mode)
+int ListDirectoryContents(wchar_t *sDir, wchar_t **name,
+  ULONGLONG *size, int mode, int *amount)
 {
-    //typedef struct file_ { wchar_t name[MAX_LEN]; ULONGLONG size } file;
-    int num = 0, i, *index;
-    wchar_t** name;
-    ULONGLONG* size;
     WIN32_FIND_DATA fdFile;
     HANDLE hFind = NULL;
-    wchar_t* sPath;
-    //index = (int*)malloc(sizeof(int) * MAX_AMOUNT);
-    index = (int*)malloc(MAX_AMOUNT * sizeof(int));
-    name = (wchar_t*)malloc(sizeof(wchar_t) * MAX_AMOUNT);
-   // size = (int*)malloc(sizeof(int) * MAX_AMOUNT);
-    size = (ULONGLONG*)malloc(MAX_AMOUNT * sizeof(ULONGLONG));
-    for (i = 0; i < MAX_AMOUNT; i++)
-        index[i] = i;
-    for (i = 0; i < MAX_AMOUNT; i++)
-        name[i] = (wchar_t*)malloc(sizeof(wchar_t) * MAX_LEN);
-    sPath = (wchar_t*)malloc(2048 * sizeof(wchar_t));
+    wchar_t *sPath;
+    sPath = (wchar_t*)malloc(MAX_LEN * sizeof(wchar_t));
     wsprintf(sPath, L"%s\\*.*", sDir);
     if ((hFind = FindFirstFileW(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
     {
-        wprintf(L"Path not found: [%s]\n", sDir);
+        wprintf(L"Путь не найден: [%s]\n", sDir);
         return 1;
     }
     do
@@ -179,87 +166,103 @@ int ListDirectoryContents(const wchar_t *sDir, int mode)
         if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp
           (fdFile.cFileName, L"..") != 0)
         {
-            ULONGLONG fileSize = fdFile.nFileSizeHigh;
-            fileSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
-            fileSize |= fdFile.nFileSizeLow;
-            wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
-            wprintf(L"File: %s\nSize: %d\n", sPath, (wchar_t)fileSize);
-            num++;
+            size[*amount] = fdFile.nFileSizeHigh;
+            size[*amount] <<= sizeof(fdFile.nFileSizeHigh) * 8;
+            size[*amount] |= fdFile.nFileSizeLow;
+            wsprintf(name[*amount], L"%s\\%s", sDir, fdFile.cFileName);
+            (*amount)++;
         }
     } while (FindNextFile(hFind, &fdFile));
     FindClose(hFind);
-   // for (i = 0; i < MAX_AMOUNT; i++)
-     //   free(name[i]);
-    //free(name);
-    free(size);
-   // free(index);
-    free(sPath + 1);
+    free(sPath);
     return 0;
 }
 void main()
 {
-    char* ta = (char*)malloc(MAX_LEN);
-    wchar_t* ca = (wchar_t*)malloc(MAX_LEN);
     setlocale(LC_ALL, "Rus");
+
     while (1)
     {
-        int mode = -1;
+        int mode, order, i, amount = 0, *index, ind = 0;
+        wchar_t *wdir, **name;
+        clock_t time;
+        double t;
+        ULONGLONG *size, *size_copy, *size_t;
+        wdir = (wchar_t*)malloc(MAX_LEN);
+        size = (ULONGLONG*)malloc(MAX_AMOUNT * sizeof(ULONGLONG));
+        size_t = (ULONGLONG*)malloc(MAX_AMOUNT * sizeof(ULONGLONG));
+        size_copy = (ULONGLONG*)malloc(MAX_AMOUNT * sizeof(ULONGLONG));
+        name = (wchar_t**)malloc(sizeof(wchar_t*) * MAX_AMOUNT);
+        index = (int*)malloc(MAX_AMOUNT * sizeof(int));
+        for (i = 0; i < MAX_AMOUNT; i++)
+            index[i] = i;
+        for (i = 0; i < MAX_AMOUNT; i++)
+            name[i] = (wchar_t*)malloc(sizeof(wchar_t) * MAX_LEN);
         printf("Выберите режим:\n0 – выбором, 1 – вставкой, "
-            "2 – пузырьком, 3 – подсчётом,\n4 – быстрая, "
-            "5 – слиянием,\n6 для выхода\n");
+          "2 – пузырьком, 3 – подсчётом,\n4 – быстрая, "
+          "5 – слиянием,\n6 для выхода\n");
         do
         {
             scanf("%d", &mode);
         } while (mode < 0 || mode > 6);
         if (mode == 6)
             return;
+        printf("0 – по возрастанию, 1 – по убыванию\n");
+        do
+        {
+            scanf("%d", &order);
+        } while (order < 0 || order > 1);
         printf("Введите адрес\n");
         getchar();
-        fgets(ta, MAX_LEN, stdin);
-        ta[strlen(ta) - 1] = '\0';
-        swprintf(ca, MAX_LEN, L"%hs", ta);
-        ListDirectoryContents(ca, mode);
+        fgetws(wdir, MAX_LEN, stdin);
+        wdir[wcslen(wdir) - 1] = '\0';
+        if (ListDirectoryContents(wdir, name, size, mode, &amount))
+            continue;
+        for (i = 0; i < amount; i++)
+            size_copy[i] = size[i];
+        for (i = 0; i < amount; i++)
+            size_t[i] = size[i];
+        time = clock();
+        switch (mode)
+        {
+            case 0:
+                ChooseSort(size_copy, amount + 1);
+                break;
+            case 1:
+                InsertSort(size_copy, amount + 1);
+                break;
+            case 2:
+                BubbleSort(size_copy, amount);
+                break;
+            case 3:
+                CountingSort(size_copy, amount);
+                break;
+            case 4:
+                QuickSort(size_copy, 0, amount - 1);
+                break;
+            case 5:
+                MergeSort(size_copy, 0, amount - 1);
+        }
+        for (ind = 0; ind < amount; ind++)
+        {
+            for (i = 0; i < amount; i++)
+                if (size_t[i] == size_copy[ind])
+                {
+                    size_t[i] = -1;
+                    break;
+                }
+            index[ind] = i;
+        }
+        time = clock() - time;
+        t = ((double)time) / CLOCKS_PER_SEC;
+        Print(name, size, index, amount, order, t);
+        for (i = 0; i < MAX_AMOUNT; i++)
+            free(name[i]);
+        free(name);
+        free(size);
+        free(size_t);
+        free(size_copy);
+        free(index);
+        free(wdir);
     }
-    free(ta);
-    free(ca);
 }
-
-/*void main()
-{
-    int mode, *a;
-    setlocale(LC_ALL, "Rus");
-    a = (int*)malloc(N * sizeof(int));
-    
-    printf("Выберите режим:\n0 – выбором, 1 – вставкой, "
-      "2 – пузырьком, 3 – подсчётом,\n4 – быстрая, "
-      "5 – слиянием,\n6 для выхода\n");
-    do
-    {
-        scanf("%d", &mode);
-    } while (mode < 0 || mode > 6);
-    Input(a, N, mode);
-    printf("\nСортировка по возрастанию\n\n");
-    switch (mode)
-    {
-    case 0:
-        ChooseSort(a, N);
-        break;
-    case 1:
-        InsertSort(a, N);
-        break;
-    case 2:
-        BubbleSort(a, N);
-        break;
-    case 3:
-        CountingSort(a, N);
-        break;
-    case 4:
-        QuickSort(a, 0, N - 1);
-        break;
-    case 5:
-        MergeSort(a, 0, N - 1);
-    }
-    Print(a, N);
-    free(a);
-}
-*/
